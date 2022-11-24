315c7e288c0999fb14c2e32d74e7d05847649a68
tasmim-concept <amina.seraoui@outlook.fr>
2022-11-24 23:26:34 +0100

    :recycle: refactor menu burger

--- assets/js/Menus/index.js
+++ assets/js/Menus/index.js
@@ -1,14 +1,11 @@
-const menuBurgers = document.querySelectorAll('.menu-burger')
+const menuBurger = document.querySelector('header')
+const close = menuBurger.querySelector('.close')
+const open = menuBurger.querySelector('.open')
 
-menuBurgers.forEach(menuBurger => {
-    const close = menuBurger.querySelector('.close')
-    const open = menuBurger.querySelector('.open')
+open.addEventListener('click', e => {
+    menuBurger.classList.add('active')
+})
 
-    open.addEventListener('click', e => {
-        menuBurger.classList.add('active')
-    })
-
-    close.addEventListener('click', e => {
-        menuBurger.classList.remove('active')
-    })
+close.addEventListener('click', e => {
+    menuBurger.classList.remove('active')
 })
