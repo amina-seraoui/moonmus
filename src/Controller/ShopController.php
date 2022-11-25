bcf49e30ee9c49ecff321539b22ebbdfd60cf7c6
tasmim-concept <amina.seraoui@outlook.fr>
2022-11-25 22:48:18 +0100

    :lipstick: shop page

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+#[Route('/boutique', name: 'shop.')]
+class ShopController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('shop/index.html.twig');
     }
 }
